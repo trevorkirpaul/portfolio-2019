fadc057e04151750ca345b8f4fc9c0b52e4e16cf
Trevor Kirpaul <trevorkirpaul@gmail.com>
2019-05-11 10:40:41 -0400

    made Text.size mandatory since optional caused a linting error

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -14,7 +14,7 @@ export interface TextProps {
   type: string
   children: React.ReactNode
   className?: string
-  size?: string
+  size: string
 }
 
 const Text = ({ type, children, className, size }: TextProps) => {
