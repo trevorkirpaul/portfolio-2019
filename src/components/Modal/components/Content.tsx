3c306a2484cea22ef93073a07f2a2cc43852139e
Trevor Kirpaul <trevorkirpaul@gmail.com>
2019-05-12 21:17:49 -0400

    - added styling for Modal component - added Backdrop component to support Modal - added styling for Modal

--- src/components/Modal/components/Content.tsx
+++ src/components/Modal/components/Content.tsx
@@ -3,12 +3,31 @@ import React from 'react'
 import * as S from '../styles'
 
 export interface ContentProps {
+  className?: string
   children: React.ReactNode
   handleClose?: () => void
+  style: any
+  background?: string
+  color?: string
 }
 
-const Content = ({ children }: ContentProps) => {
-  return <S.Content>{children}</S.Content>
+const Content = ({
+  className,
+  children,
+  style,
+  background,
+  color
+}: ContentProps) => {
+  return (
+    <S.Content
+      className={className}
+      style={style}
+      background={background}
+      color={color}
+    >
+      {children}
+    </S.Content>
+  )
 }
 
 export default Content
--- src/stories/components/Modal.story.tsx
+++ src/stories/components/Modal.story.tsx
@@ -1,14 +1,44 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import styled from 'styled-components'
 
 import Modal from 'components/Modal'
 import TitlePanel from 'components/TitlePanel'
 import Button from 'components/Button'
+import Text from 'components/Text'
 
 import themeDecorator from '../themeDecorator'
 import addDivDecorator from '../addDivDecorator'
 
 const TOAST_ROOT = 'MODAL-ROOT'
+
+const StyledText = styled(Text)`
+  color: #f8f8f8;
+`
+
+const StyledTextDark = styled(Text)`
+  color: #383838;
+`
+const StyledButton = styled(Button)`
+  color: #383838;
+  background: palegoldenrod;
+  border-radius: 5px;
+  padding: 5px 10px;
+`
+const StyledButtonDark = styled(Button)`
+  color: #f8f8f8;
+  background: #383838;
+  border-radius: 5px;
+  padding: 5px 10px;
+`
+
+const StyledModal = styled(Modal)`
+  background: paleturquoise;
+  padding: 25px;
+  border-radius: 5px;
+  /* box-shadow: 3px 3px 5px 6px #ccc; */
+  box-shadow: 0 10px 6px -6px #777;
+`
 
 const BaseModalWithState = () => {
   const [visible, toggleVisibility] = React.useState(false)
@@ -21,12 +51,60 @@ const BaseModalWithState = () => {
         domNodeID='MODAL-ROOT'
         handleClose={() => toggleVisibility(!visible)}
       >
-        <Modal.Content>
-          <TitlePanel title='Modal Title' subTitle='Base Modal' />
+        <TitlePanel title='Modal Title' subTitle='Base Modal' />
 
-          <Button onClick={() => toggleVisibility(!visible)}>Close</Button>
-        </Modal.Content>
+        <Button onClick={() => toggleVisibility(!visible)}>Close</Button>
       </Modal>
+    </React.Fragment>
+  )
+}
+
+const ModalUsingBackgroundAndColorWithState = () => {
+  const [visible, toggleVisibility] = React.useState(false)
+
+  return (
+    <React.Fragment>
+      <Button onClick={() => toggleVisibility(!visible)}>Show Modal</Button>
+      <Modal
+        visible={visible}
+        domNodeID='MODAL-ROOT'
+        handleClose={() => toggleVisibility(!visible)}
+        background='#383838'
+        color='#f8f8f8'
+      >
+        <StyledText>
+          This Modal sets the background color using `props.background`
+        </StyledText>
+        <StyledText>
+          It also sets the color but it's not used since we're using Text
+          components
+        </StyledText>
+        <StyledButton onClick={() => toggleVisibility(!visible)}>
+          Close
+        </StyledButton>
+      </Modal>
+    </React.Fragment>
+  )
+}
+
+const CustomStyledModalWithState = () => {
+  const [visible, toggleVisibility] = React.useState(true)
+
+  return (
+    <React.Fragment>
+      <Button onClick={() => toggleVisibility(!visible)}>Show Modal</Button>
+      <StyledModal
+        visible={visible}
+        domNodeID='MODAL-ROOT'
+        handleClose={() => toggleVisibility(!visible)}
+      >
+        <StyledTextDark>
+          This Modal sets the background color using `props.background`
+        </StyledTextDark>
+        <StyledButtonDark onClick={() => toggleVisibility(!visible)}>
+          Close
+        </StyledButtonDark>
+      </StyledModal>
     </React.Fragment>
   )
 }
@@ -35,3 +113,7 @@ storiesOf('Modal', module)
   .addDecorator(storyFn => addDivDecorator(storyFn, TOAST_ROOT))
   .addDecorator(storyFn => themeDecorator(storyFn))
   .add('base', () => <BaseModalWithState />)
+  .add('using background and color props', () => (
+    <ModalUsingBackgroundAndColorWithState />
+  ))
+  .add('custom styles', () => <CustomStyledModalWithState />)
